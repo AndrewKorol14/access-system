bafbe2cbf0016149d0a14e19b46d6d9bf5c1993d
AndrewKorol <korol.andrew14@gmail.com>
2018-11-07 12:46:47 +0300

    Change names of elements on start window

--- access-system/StartForm.Designer.cs
+++ access-system/StartForm.Designer.cs
@@ -30,11 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.новоеЗданиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.открытьЗданиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newBuildingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openBuildingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
@@ -44,48 +44,48 @@
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.AliceBlue;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.файлToolStripMenuItem,
-            this.справкаToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(557, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // файлToolStripMenuItem
+            // fileToolStripMenuItem
             // 
-            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.новоеЗданиеToolStripMenuItem,
-            this.открытьЗданиеToolStripMenuItem});
-            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-            this.файлToolStripMenuItem.Text = "Файл";
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newBuildingToolStripMenuItem,
+            this.openBuildingToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
             // 
-            // справкаToolStripMenuItem
+            // newBuildingToolStripMenuItem
             // 
-            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.оПрограммеToolStripMenuItem});
-            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
-            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.справкаToolStripMenuItem.Text = "Справка";
+            this.newBuildingToolStripMenuItem.Name = "newBuildingToolStripMenuItem";
+            this.newBuildingToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newBuildingToolStripMenuItem.Text = "New building";
             // 
-            // новоеЗданиеToolStripMenuItem
+            // openBuildingToolStripMenuItem
             // 
-            this.новоеЗданиеToolStripMenuItem.Name = "новоеЗданиеToolStripMenuItem";
-            this.новоеЗданиеToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
-            this.новоеЗданиеToolStripMenuItem.Text = "Новое здание";
+            this.openBuildingToolStripMenuItem.Name = "openBuildingToolStripMenuItem";
+            this.openBuildingToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openBuildingToolStripMenuItem.Text = "Open building";
             // 
-            // открытьЗданиеToolStripMenuItem
+            // helpToolStripMenuItem
             // 
-            this.открытьЗданиеToolStripMenuItem.Name = "открытьЗданиеToolStripMenuItem";
-            this.открытьЗданиеToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
-            this.открытьЗданиеToolStripMenuItem.Text = "Открыть здание";
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
             // 
-            // оПрограммеToolStripMenuItem
+            // aboutToolStripMenuItem
             // 
-            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
-            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
             // 
             // button1
             // 
@@ -95,7 +95,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 71);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Новое здание";
+            this.button1.Text = "New building";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
@@ -106,11 +106,11 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(98, 71);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Открыть здание";
+            this.button2.Text = "Open building";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // StartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -121,7 +121,7 @@
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
+            this.Name = "StartForm";
             this.Text = "Access system v1.0.0";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -133,11 +133,11 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem новоеЗданиеToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem открытьЗданиеToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newBuildingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openBuildingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
