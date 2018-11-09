e44e43edd7edc3bbfccb582d48703353655577d6
AndrewKorol <korol.andrew14@gmail.com>
2018-11-10 01:04:47 +0300

    Add buttons for view visiting log and electronic pass

--- access-system/SecurityPostForm.Designer.cs
+++ access-system/SecurityPostForm.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -39,10 +41,9 @@
             this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(312, 54);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(233, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 47);
+            this.button1.Size = new System.Drawing.Size(139, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Create card";
             this.button1.UseVisualStyleBackColor = false;
@@ -53,10 +54,9 @@
             this.button2.BackColor = System.Drawing.Color.Black;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(312, 296);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(232, 310);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(185, 46);
+            this.button2.Size = new System.Drawing.Size(139, 37);
             this.button2.TabIndex = 1;
             this.button2.Text = "Emulate entrance";
             this.button2.UseVisualStyleBackColor = false;
@@ -66,25 +66,49 @@
             this.button3.BackColor = System.Drawing.Color.Black;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(312, 171);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(235, 88);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(184, 46);
+            this.button3.Size = new System.Drawing.Size(138, 37);
             this.button3.TabIndex = 2;
             this.button3.Text = "Lock/Unlock card";
             this.button3.UseVisualStyleBackColor = false;
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(235, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Show visiting log";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(233, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(138, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Show all electronic pass";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
             // SecurityPostForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(743, 442);
+            this.ClientSize = new System.Drawing.Size(557, 359);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SecurityPostForm";
             this.Text = "Security post";
             this.ResumeLayout(false);
@@ -96,5 +120,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
