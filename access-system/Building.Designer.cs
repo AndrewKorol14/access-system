182fdd6bc3d45cec93a2632d01a3ed5c38f32e64
AndrewKorol <korol.andrew14@gmail.com>
2018-11-09 22:21:09 +0300

    Add floor form

---
+++ access-system/Building.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace access_system
+{
+    partial class Building
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Building));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(222, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name of building:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button1.Location = new System.Drawing.Point(42, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(475, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Floor 1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button2.Location = new System.Drawing.Point(42, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(475, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Floor 2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button3.Location = new System.Drawing.Point(42, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(475, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Floor 3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button4.Location = new System.Drawing.Point(42, 237);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(475, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Floor 4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Building
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(557, 359);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Building";
+            this.Text = "Building";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ access-system/BuildingTableCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_system
+{
+    public class BuildingTableCreator
+    {
+        public int[,] buildingMatrix;
+
+        public BuildingTableCreator(int n, int m)
+        {
+            buildingMatrix = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    buildingMatrix[i, j] = (i + 1) * 100 + j; 
+                }
+            }
+        }
+    }
+}
