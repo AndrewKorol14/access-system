81416c3ea80b89328e20da260247be8bc8574de5
YauheniLoban <zhenya-loban@mail.ru>
2018-11-09 22:45:45 +0300

    add stale fo Building form

--- access-system/NewBuildingCreationForm.Designer.cs
+++ access-system/NewBuildingCreationForm.Designer.cs
@@ -49,7 +49,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.label4.Location = new System.Drawing.Point(147, 35);
+            this.label4.Location = new System.Drawing.Point(106, 30);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(335, 18);
             this.label4.TabIndex = 1;
