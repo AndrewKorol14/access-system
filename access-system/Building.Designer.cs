d96fcb01275a5a5b2e08dedea4e82338919d645e
AndrewKorol <korol.andrew14@gmail.com>
2018-11-18 13:08:04 +0300

    Change background for building and card creation forms

--- access-system/Building.Designer.cs
+++ access-system/Building.Designer.cs
@@ -39,11 +39,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(292, 23);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 22);
+            this.label1.Size = new System.Drawing.Size(131, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Name of building:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -51,39 +51,36 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button1.Location = new System.Drawing.Point(135, 66);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(101, 54);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(487, 47);
+            this.button1.Size = new System.Drawing.Size(365, 38);
             this.button1.TabIndex = 1;
             this.button1.Text = "Floor 1";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(135, 135);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(101, 110);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(487, 55);
+            this.button2.Size = new System.Drawing.Size(365, 41);
             this.button2.TabIndex = 2;
             this.button2.Text = "Floor 2";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.BackColor = System.Drawing.Color.DarkOrange;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(135, 214);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(101, 170);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(487, 46);
+            this.button3.Size = new System.Drawing.Size(365, 41);
             this.button3.TabIndex = 3;
             this.button3.Text = "Floor 3";
             this.button3.UseVisualStyleBackColor = false;
@@ -91,13 +88,12 @@
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.BackColor = System.Drawing.Color.DarkOrange;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(135, 292);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(101, 237);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(487, 47);
+            this.button4.Size = new System.Drawing.Size(365, 41);
             this.button4.TabIndex = 4;
             this.button4.Text = "Floor 4";
             this.button4.UseVisualStyleBackColor = false;
@@ -105,18 +101,17 @@
             // 
             // Building
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(731, 436);
+            this.ClientSize = new System.Drawing.Size(548, 354);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Building";
             this.Text = "Building";
             this.Load += new System.EventHandler(this.Building_Load);
