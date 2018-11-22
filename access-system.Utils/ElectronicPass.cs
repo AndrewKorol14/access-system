d005e7f88ccfd86cc340f8e1c801ca8c583472fd
YauheniLoban <zhenya-loban@mail.ru>
2018-11-22 18:21:10 +0300

    add user class and electronicPass class

---
+++ access-system.Utils/ElectronicPass.cs
@@ -0,0 +1,33 @@
+﻿using access_system.Utils.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_system.Utils
+{
+    /// <summary>
+    /// Description of the electronic pass
+    /// </summary>
+    public class ElectronicPass
+    {
+        private PassBlocking blockingStatus;
+        private readonly int cardNumber;
+
+        public ElectronicPass(PassBlocking blockingStatus, int cardNumber)
+        {
+            this.blockingStatus = blockingStatus;
+            this.cardNumber = cardNumber;
+        }
+
+        /// <summary>
+        /// Chanhed card blocking status on new
+        /// </summary>
+        /// <param name="newStatus"></param>
+        public void СhangeСardStatus(PassBlocking newStatus)
+        {
+            blockingStatus = newStatus;
+        }
+    }
+}
---
+++ access-system.Utils/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_system.Utils
+{
+    /// <summary>
+    /// Provides a user description in the system.
+    /// </summary>
+    public class User
+    {
+        private string FirsName;
+        private string LastName;
+        private List<ElectronicPass> electronicPasses;
+        private readonly int uniqeID;
+
+        public User(string FirsName, string LastName, List<ElectronicPass> electronicPasses, int uniqeID)
+        {
+            this.electronicPasses = electronicPasses;
+            this.FirsName = FirsName;
+            this.LastName = LastName;
+            this.uniqeID = uniqeID;
+        }
+
+        public User(string FirsName, string LastName, ElectronicPass electronicPass, int uniqeID)
+        {
+            this.FirsName = FirsName;
+            this.LastName = LastName;
+            this.uniqeID = uniqeID;
+            electronicPasses = new List<ElectronicPass>();
+            electronicPasses.Add(electronicPass);
+        }
+
+        /// <summary>
+        /// Add user additional card
+        /// </summary>
+        /// <param name="electronicPass"></param>
+        public void AddPassForUser(ElectronicPass electronicPass)
+        {
+            electronicPasses.Add(electronicPass);
+        }
+    }
+}
