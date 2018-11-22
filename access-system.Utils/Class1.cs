e15f1e0eb9034c261e0b1f8d8a9fe503657e0e48
YauheniLoban <zhenya-loban@mail.ru>
2018-11-22 17:14:55 +0300

    add project for class

---
+++ access-system.Utils/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access_system.Utils
+{
+    public class Class1
+    {
+    }
+}
