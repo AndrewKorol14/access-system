6111bb13ac7fe0494811321b53087c0065eac7b0
YauheniLoban <zhenya-loban@mail.ru>
2018-11-22 17:31:29 +0300

    add enums for room and user

---
+++ access-system.Utils/Enums/RoomTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace access_system.Utils.Enums
+{
+    /// <summary>
+    /// RoomTypes enum. All types of rooms available in the building
+    /// </summary>
+    public enum RoomTypes
+    {
+        OfficeSpace,
+        Classroom,
+        LectureRoom,
+        GeneralPurpose,
+        Laboratory,
+        StaffOnly
+    }
+}
---
+++ access-system.Utils/Enums/UserTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace access_system.Utils.Enums
+{
+    /// <summary>
+    /// RoomTypes enum. All types of users who may be in the building
+    /// </summary>
+    public enum UserTypes
+    {
+        Teacher,
+        ServiceStaff,
+        Administration,
+        Security,
+        Student
+    }
+}
