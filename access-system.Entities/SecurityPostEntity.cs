58ee16e3834a3fda6a2ce317ce6cc58fafbf9ba0
AndrewKorol <korol.andrew14@gmail.com>
2018-12-03 00:15:53 +0300

    Add entities to entity project

---
+++ access-system.Entities/SecurityPostEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace access_system.Entities
+{
+    class SecurityPostEntity
+    {
+        private List<User> _users;
+        private List<ElectronicPass> _electronicPasses;
+
+        public SecurityPostEntity()
+        {
+            _users = new List<User>();
+            _electronicPasses = new List<ElectronicPass>();
+        }
+    }
+}
