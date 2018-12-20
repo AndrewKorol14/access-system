210a65a58941741fec968a98421bad199851abc5
AndrewKorol <korol.andrew14@gmail.com>
2018-12-20 22:08:27 +0300

    Add load path folder from dialog

--- access-system/StartForm.cs
+++ access-system/StartForm.cs
@@ -1,67 +1,36 @@
-﻿//using access_system.Entities.UI;
-//using access_system.Presenters.Views;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace access_system
 {
-    public partial class StartForm : Form//, IStartFormView
+    public partial class StartForm : Form
     {
-        private readonly int windowHeight = 460;
-        private readonly int windowWidth = 710;
+        private readonly int _windowHeight = 460;
+        private readonly int _windowWidth = 710;
 
         public StartForm()
         {
             InitializeComponent();
-            this.MinimumSize = new System.Drawing.Size(windowWidth, windowHeight);
-            this.MaximumSize = new System.Drawing.Size(windowWidth, windowHeight);
+            this.MinimumSize = new System.Drawing.Size(_windowWidth, _windowHeight);
+            this.MaximumSize = new System.Drawing.Size(_windowWidth, _windowHeight);
             this.MaximizeBox = false;
-        }
+        } 
 
-        
-        
-
-        private void button2_Click(object sender, EventArgs e)
+        private void openBuildingButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JSON(*.json)|*.json";
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-
+                string filename = ofd.FileName;
             }
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void newBuildingButton_Click(object sender, EventArgs e)
         {
             NewBuildingCreationForm newBuildingCreationForm = new NewBuildingCreationForm();
             newBuildingCreationForm.Show();
             Hide();
-        }
-
-        private void StartForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void newBuildingToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            NewBuildingCreationForm newBuildingCreationForm = new NewBuildingCreationForm();
-            newBuildingCreationForm.Show();
-        }
-
-        private void openBuildingToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-
-            }
         }
     }
 }
